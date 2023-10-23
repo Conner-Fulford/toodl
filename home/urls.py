f803c9abb279f8faf6a0b311fa98c7a594661af0
Chandler <89811827+4am-walking@users.noreply.github.com>
2023-10-23 19:10:05 +0000

    initial commit

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
 
-# Uncomment and edit this when adding new templates
-'''
 urlpatterns = [
-path("", views.index, name="index"),
+path("", views.login, name="login"),
 ]
-'''
--- home/views.py
+++ home/views.py
@@ -1,10 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Uncomment and edit this when adding new templates
-'''
 # Create your views here.
-def index(request):
+def login(request):
     # Render the HTML template index.html
-    return render(request, 'index.html')
-'''
+    return render(request, 'login.html')
