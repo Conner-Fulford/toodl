6b42cf8f9251bcc11b4c43634cae866794d5f4a8
Chandler <89811827+4am-walking@users.noreply.github.com>
2023-10-23 18:53:23 +0000

    removed index.html

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# Uncomment and edit this when adding new templates
+'''
 urlpatterns = [
 path("", views.index, name="index"),
 ]
+'''
--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Uncomment and edit this when adding new templates
+'''
 # Create your views here.
 def index(request):
     # Render the HTML template index.html
     return render(request, 'index.html')
+'''
