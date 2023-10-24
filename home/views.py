1066d0e5986dc07717b6c04fcb3bd0ea4a1a697c
Chandler <89811827+4am-walking@users.noreply.github.com>
2023-10-24 13:54:14 +0000

    initial commit

--- home/views.py
+++ home/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+def calendar(request):
     # Render the HTML template index.html
-    return render(request, 'index.html')
+    return render(request, 'calendar.html')
