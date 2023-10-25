61753027bacf57955953a1291f3f9c78bc4385a4
Damion Mason <dmason@checkintocash.com>
2023-10-25 12:37:08 -0400

    db settings via manage.py

--- toodl/settings.py
+++ toodl/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = "toodl.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": BASE_DIR / "toodlapp",
+        "USER": 'admin',
+        "PASSWORD": 'admin',
+        "HOST": 'localhost',
+        "PORT": '',
     }
 }
 
