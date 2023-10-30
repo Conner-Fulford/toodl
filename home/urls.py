288baa2f955e1c231daef264f031e2ded59d2abb
Chandler <89811827+4am-walking@users.noreply.github.com>
2023-10-30 14:38:43 +0000

    fixed pr disaster

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path("", views.calendar, name="calendar"),
-path("", views.index, name="index"),
+    path("", views.index, name="index"),
+    path("calendar/", views.calendar, name="calendar"),
 ]
--- toodl/urls.py
+++ toodl/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("calendar/", include('home.urls')),
     path("admin/", admin.site.urls),
+    path("", include('home.urls')),
+    path("", include('home.urls')),
 ]
