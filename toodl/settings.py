245c076c2e4650c93f0221e1cc1a4d965516dda0
Chandler <89811827+4am-walking@users.noreply.github.com>
2023-10-30 14:16:23 +0000

    fixed pr disaster

--- toodl/settings.py
+++ toodl/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["toodl.azurewebsites.net"]
+ALLOWED_HOSTS = ["toodl.azurewebsites.net", "127.0.0.1"]
 
 
 # Application definition
