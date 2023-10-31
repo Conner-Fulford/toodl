af7bb4c546d484c47bb859c250ff3b92e1505e56
Chandler <89811827+4am-walking@users.noreply.github.com>
2023-10-31 21:38:49 +0000

    only authenticated users can access calendar + minor aesthetic additions

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("login/", views.login, name="login"),
+    path("login/", views.loginPage, name="login"),
     path("register/", views.register, name="register"),
     path("", views.index, name="index"),
     path("calendar/", views.calendar, name="calendar"),
--- home/views.py
+++ home/views.py
@@ -1,25 +1,28 @@
 from django.db import IntegrityError
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User, auth
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.contrib import messages
 
 
 # Create your views here.
 def calendar(request):
+    if not request.user.is_authenticated:
+        return redirect("/login")
     # Render the HTML template index.html
     return render(request, "calendar.html")
 
 
 # Create your views here.
-def login(request):
+def loginPage(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
 
-        user = auth.authenticate(username=username, password=password)
+        user = authenticate(username=username, password=password)
 
         if user is not None:
-            auth.login(request, user)
+            login(request, user)
             return redirect("/calendar")
         else:
             messages.info(request, "Invalid username or password")
