7066c4acd076136b1613d6e3003a5c859b97de77
Chandler <89811827+4am-walking@users.noreply.github.com>
2023-10-31 21:11:49 +0000

    added basic user auth and error check for if user exists

--- home/views.py
+++ home/views.py
@@ -1,18 +1,59 @@
-from django.shortcuts import render
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
 
 # Create your views here.
 def calendar(request):
     # Render the HTML template index.html
-    return render(request, 'calendar.html')
+    return render(request, "calendar.html")
+
 
 # Create your views here.
 def login(request):
-    # Render the HTML template index.html
-    return render(request, 'login.html')
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/calendar")
+        else:
+            messages.info(request, "Invalid username or password")
+            return redirect("/login")
+    else:
+        # Render the HTML template index.html
+        return render(request, "login.html")
+
 
 def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        retype_password = request.POST["retype_password"]
+        terms = request.POST.get("terms")
+        try:
+            if terms is not None and retype_password == password:
+                user = User.objects.create_user(
+                    username=username, password=password, email=email
+                )
+                user.save()
+                print(f"User {user.username} created.")
+                return redirect("/login")
+        except IntegrityError as error:
+            error_message = str(error)
+            if "unique constraint" in error_message and "username" in error_message:
+                messages.info(request, "Username already exits.")
+            else:
+                messages.info(request, "An error occurred while reigstering.")
+
     # Render the HTML template index.html
-    return render(request, 'register.html')
+    return render(request, "register.html")
+
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
