d377c3d772984e5348aff4947ec8a41b48a7cc17
Chandler <89811827+4am-walking@users.noreply.github.com>
2023-11-01 20:16:38 +0000

    testing

--- toodl/settings.py
+++ toodl/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["toodl.azurewebsites.net", "127.0.0.1"]
 
