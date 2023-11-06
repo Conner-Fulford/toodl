44d42e23a104c87a0b42e9e5329b26304528135b
Chandler <89811827+4am-walking@users.noreply.github.com>
2023-11-06 19:42:01 +0000

    testing for user auth fields

--- home/tests/test_models.py
+++ home/tests/test_models.py
@@ -9,23 +9,20 @@ class CustomUserTest(TestCase):
             username="testcase", email="testcase@testcase.com", password="testcase123!"
         )
 
+    def test_username(self):
+        customuser = CustomUser.objects.get(id=1)
+        field_label = customuser._meta.get_field("username").verbose_name
+        print(field_label)
+        self.assertEqual(field_label, "username")
 
-def test_username(self):
-    customuser = CustomUser.objects.get(id=1)
-    field_label = customuser._meta.get_field("username").verbose_name
-    print(field_label)
-    self.assertEqual(field_label, "username")
+    def test_email(self):
+        customuser = CustomUser.objects.get(id=1)
+        field_label = customuser._meta.get_field("email").verbose_name
+        print(field_label)
+        self.assertEqual(field_label, "email")
 
-
-def test_email(self):
-    customuser = CustomUser.objects.get(id=1)
-    field_label = customuser._meta.get_field("email").verbose_name
-    print(field_label)
-    self.assertEqual(field_label, "email")
-
-
-def test_password(self):
-    customeruser = CustomUser.objects.get(id=1)
-    field_label = customeruser._meta.get_field("password").verbose_name
-    print(field_label)
-    self.assertEqual(field_label, "password")
+    def test_password(self):
+        customeruser = CustomUser.objects.get(id=1)
+        field_label = customeruser._meta.get_field("password").verbose_name
+        print(field_label)
+        self.assertEqual(field_label, "password")
