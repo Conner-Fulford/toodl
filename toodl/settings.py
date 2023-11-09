e102cd649c4b5da5478d586081a322cc46e3507b
Conner-Fulford <66020351+Conner-Fulford@users.noreply.github.com>
2023-11-09 14:38:43 +0000

    updated settings

--- toodl/settings.py
+++ toodl/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["toodl.azurewebsites.net"]
+ALLOWED_HOSTS = ["toodl.azurewebsites.net", "127.0.0.1"]
 
 
 # Application definition
