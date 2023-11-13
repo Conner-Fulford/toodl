ed77688bbb098cd293e99760d6b107170e090fb9
Eric <70559425+utchaozhou@users.noreply.github.com>
2023-11-13 03:01:22 +0000

    updated the add event form and performed checks

--- home/models.py
+++ home/models.py
@@ -2,17 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class Event(models.Model):
-    """Class that holds the schema for the Event table"""
-
-    id = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=200)
-    description = models.CharField(max_length=2000)
-    startTime = models.DateTimeField()
-    endTime = models.DateTimeField()
-
-
 class CustomUser(AbstractUser):
     """Class that overrides User to force emails to be unique"""
 
     email = models.EmailField(unique=True)
+
+
+class Event(models.Model):
+    """Class that holds the schema for the Event table"""
+
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, blank=True)
+    description = models.CharField(max_length=2000, blank=True)
+    startTime = models.DateTimeField(blank=True)
+    endTime = models.DateTimeField(blank=True)
+
+    objects = models.Manager()
