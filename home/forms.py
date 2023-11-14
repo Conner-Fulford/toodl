57a4a41ad3789f6adcd6b446e5aa57aeb712e6ad
Chandler <89811827+4am-walking@users.noreply.github.com>
2023-11-14 19:40:38 +0000

    added style chages, ability to see multiple months, ability to click on a day in the calendar, and user-friendly date selection

--- home/forms.py
+++ home/forms.py
@@ -6,3 +6,18 @@ class EventForm(forms.ModelForm):
     class Meta:
         model = Event
         fields = ["title", "description", "startTime", "endTime"]
+        widgets = {
+            "title": forms.TextInput(attrs={"required": "required"}),
+            "description": forms.Textarea(
+                attrs={"rows": 4, "style": "resize: vertical;"}
+            ),
+            "startTime": forms.DateTimeInput(
+                attrs={"type": "datetime-local", "required": "required"}
+            ),
+            "endTime": forms.DateTimeInput(
+                attrs={"type": "datetime-local", "required": "required"}
+            ),
+        }
+        labels = {
+            "description": "Description",  # Set the label for the description field
+        }
