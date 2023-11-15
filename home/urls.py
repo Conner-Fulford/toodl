343aa51418f97ffab7f48ed78c64ba54adc79ad8
Chandler <89811827+4am-walking@users.noreply.github.com>
2023-11-15 19:55:45 +0000

    added ability to export events

--- home/urls.py
+++ home/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("logout/", views.logout_view, name="logout"),
     path("get_events/", views.get_events, name="get_events"),
     path("import_events/", views.import_events, name="import_events"),
+    path("export_events/", views.export_events, name="export_events"),
     path("delete_event/<int:event_id>/", views.delete_event, name="delete_event"),
 ]
